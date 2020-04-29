11ca8f62f46df33e5e77a901914063d8518953d9
s15852 <s15852@pjwstk.edu.pl>
2020-04-29 15:02:37 +0200

    Getting list of events from database through API.

--- app/src/main/java/com/example/ticket_machine/ui/events/EventsFragment.java
+++ app/src/main/java/com/example/ticket_machine/ui/events/EventsFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,11 +13,26 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.ticket_machine.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EventsFragment extends Fragment {
 
     private EventsViewModel eventsViewModel;
+    private static String URL_EVENTS;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +46,52 @@ public class EventsFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        URL_EVENTS = getString(R.string.URL_EVENTS);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_EVENTS,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+                            JSONArray jsonArray = jsonObject.getJSONArray("read");
+
+                            if (success.equals("1")) {
+                                for (int i = 0; i < jsonArray.length(); i++) {
+                                    JSONObject object = jsonArray.getJSONObject(i);
+
+                                    String name = object.getString("name").trim();
+                                    String description = object.getString("description").trim();
+                                    String price = object.getString("price").trim();
+
+                                    Toast.makeText(getContext(),
+                                            "Events: \n" +
+                                                    "Event name : " + name +
+                                                    "\nEvent description : " + description +
+                                                    "\nEvent price : " + price
+                                            , Toast.LENGTH_LONG)
+                                            .show();
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getContext(), "Events List Error" + e.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(), "Events List Error" + error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(stringRequest);
+
         return root;
     }
 }
