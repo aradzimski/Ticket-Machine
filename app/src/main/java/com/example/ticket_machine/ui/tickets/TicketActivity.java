5ffd7b3bf66ae26e5f23d2b532c579e0cf590693
s15852 <s15852@pjwstk.edu.pl>
2020-05-28 16:25:42 +0200

    Ticket list and details with QR Code as new activity

---
+++ app/src/main/java/com/example/ticket_machine/ui/tickets/TicketActivity.java
@@ -0,0 +1,60 @@
+package com.example.ticket_machine.ui.tickets;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.ticket_machine.R;
+import com.example.ticket_machine.TicketsActivity;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.MenuItem;
+
+public class TicketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_detail);
+
+        // savedInstanceState is non-null when there is fragment state
+        // saved from previous configurations of this activity
+        // (e.g. when rotating the screen from portrait to landscape).
+        // In this case, the fragment will automatically be re-added
+        // to its container so we don't need to manually add it.
+        // For more information, see the Fragments API guide at:
+        //
+        // http://developer.android.com/guide/components/fragments.html
+        //
+        if (savedInstanceState == null) {
+            // Create the detail fragment and add it to the activity
+            // using a fragment transaction.
+            Bundle arguments = new Bundle();
+            arguments.putString(TicketFragment.ARG_ITEM_ID,
+                    getIntent().getStringExtra(TicketFragment.ARG_ITEM_ID));
+            TicketFragment fragment = new TicketFragment();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.item_detail_container, fragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            // This ID represents the Home or Up button. In the case of this
+            // activity, the Up button is shown. For
+            // more details, see the Navigation pattern on Android Design:
+            //
+            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+            //
+            navigateUpTo(new Intent(this, TicketsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
