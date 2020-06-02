df2eb986dd6de9f28e7e2b0d2b87cc68b420ab90
s15852 <s15852@pjwstk.edu.pl>
2020-06-02 13:40:56 +0200

    Camera surface and QR Code Scanner

--- app/src/main/java/com/example/ticket_machine/ui/scanner/ScannerFragment.java
+++ app/src/main/java/com/example/ticket_machine/ui/scanner/ScannerFragment.java
@@ -1,12 +1,18 @@
 package com.example.ticket_machine.ui.scanner;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.android.volley.AuthFailureError;
@@ -18,11 +24,16 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.ticket_machine.R;
 import com.example.ticket_machine.models.Event;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,6 +42,10 @@ public class ScannerFragment extends Fragment {
     public static final String ARG_ITEM_ID = "item_id";
     private static String URL_GETEVENT;
     private Event mItem;
+    SurfaceView surfaceView;
+    CameraSource cameraSource;
+    TextView resultText;
+    BarcodeDetector barcodeDetector;
 
     public ScannerFragment() {
     }
@@ -43,7 +58,60 @@ public class ScannerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-         final View rootView = inflater.inflate(R.layout.scanner_detail, container, false);
+        final View rootView = inflater.inflate(R.layout.scanner_detail, container, false);
+
+        surfaceView = (SurfaceView) rootView.findViewById(R.id.camera_preview);
+        resultText = (TextView) rootView.findViewById(R.id.scan_result);
+
+        barcodeDetector = new BarcodeDetector.Builder(getContext())
+                .setBarcodeFormats(Barcode.QR_CODE).build();
+        cameraSource = new CameraSource.Builder(getContext(), barcodeDetector)
+                .setRequestedPreviewSize(1920, 1080).build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                try {
+                    cameraSource.start(holder);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> qrCodes = detections.getDetectedItems();
+
+                if (qrCodes.size() != 0) {
+                    resultText.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            resultText.setText(qrCodes.valueAt(0).displayValue);
+                        }
+                    });
+                }
+            }
+        });
 
         if (getArguments().containsKey(ARG_ITEM_ID)) {
 
@@ -73,22 +141,21 @@ public class ScannerFragment extends Fragment {
                                         }
                                     }
                                 }
-                            } catch (JSONException e){
+                            } catch (JSONException e) {
                                 e.printStackTrace();
-                                Toast.makeText(getContext(),"Cannot get event. Error: " + e.toString(),Toast.LENGTH_LONG).show();
+                                Toast.makeText(getContext(), "Cannot get event. Error: " + e.toString(), Toast.LENGTH_LONG).show();
                             }
                         }
                     },
                     new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(getContext(),"Cannot get event. Error: " + error.toString(),Toast.LENGTH_LONG).show();
+                            Toast.makeText(getContext(), "Cannot get event. Error: " + error.toString(), Toast.LENGTH_LONG).show();
                         }
-                    })
-            {
+                    }) {
                 @Override
                 protected Map<String, String> getParams() throws AuthFailureError {
-                    Map<String,String> params = new HashMap<>();
+                    Map<String, String> params = new HashMap<>();
                     params.put("id", getArguments().getString(ARG_ITEM_ID));
                     return params;
                 }
