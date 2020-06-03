cffac259def820b7777e9ce5c493f6b2b4786187
s15852 <s15852@pjwstk.edu.pl>
2020-06-03 18:37:32 +0200

    Camera surface and QR Code Scanner

--- app/src/main/java/com/example/ticket_machine/ui/scanner/ScannerFragment.java
+++ app/src/main/java/com/example/ticket_machine/ui/scanner/ScannerFragment.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.android.volley.AuthFailureError;
@@ -41,11 +42,14 @@ public class ScannerFragment extends Fragment {
 
     public static final String ARG_ITEM_ID = "item_id";
     private static String URL_GETEVENT;
+    private static String URL_GETTICKETBYKEY;
+    private static String URL_UPDATETICKETSTATUS;
+    private String lastScannedKey;
     private Event mItem;
-    SurfaceView surfaceView;
-    CameraSource cameraSource;
-    TextView resultText;
-    BarcodeDetector barcodeDetector;
+    private SurfaceView surfaceView;
+    private CameraSource cameraSource;
+    private TextView resultText;
+    private BarcodeDetector barcodeDetector;
 
     public ScannerFragment() {
     }
@@ -60,62 +64,25 @@ public class ScannerFragment extends Fragment {
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.scanner_detail, container, false);
 
+        URL_GETTICKETBYKEY = getString(R.string.URL_GETTICKETBYKEY);
+        URL_GETEVENT = getString(R.string.URL_GETEVENT);
+        URL_UPDATETICKETSTATUS = getString(R.string.URL_UPDATETICKETSTATUS);
+
         surfaceView = (SurfaceView) rootView.findViewById(R.id.camera_preview);
         resultText = (TextView) rootView.findViewById(R.id.scan_result);
+
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA},
+                    50);
+        }
 
         barcodeDetector = new BarcodeDetector.Builder(getContext())
                 .setBarcodeFormats(Barcode.QR_CODE).build();
         cameraSource = new CameraSource.Builder(getContext(), barcodeDetector)
                 .setRequestedPreviewSize(1920, 1080).build();
 
-        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder holder) {
-                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    return;
-                }
-                try {
-                    cameraSource.start(holder);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-                cameraSource.stop();
-            }
-        });
-
-        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
-            @Override
-            public void release() {
-
-            }
-
-            @Override
-            public void receiveDetections(Detector.Detections<Barcode> detections) {
-                final SparseArray<Barcode> qrCodes = detections.getDetectedItems();
-
-                if (qrCodes.size() != 0) {
-                    resultText.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            resultText.setText(qrCodes.valueAt(0).displayValue);
-                        }
-                    });
-                }
-            }
-        });
-
         if (getArguments().containsKey(ARG_ITEM_ID)) {
-
-            URL_GETEVENT = getString(R.string.URL_GETEVENT);
 
             StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_GETEVENT,
                     new Response.Listener<String>() {
@@ -164,6 +131,104 @@ public class ScannerFragment extends Fragment {
             RequestQueue requestQueue = Volley.newRequestQueue(getContext());
             requestQueue.add(stringRequest);
         }
+
+        lastScannedKey = "";
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                try {
+                    cameraSource.start(holder);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> qrCodes = detections.getDetectedItems();
+
+                if (qrCodes.size() != 0) {
+                    if (!lastScannedKey.equals(qrCodes.valueAt(0).rawValue)) {
+                        resultText.post(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_GETTICKETBYKEY,
+                                        new Response.Listener<String>() {
+                                            @Override
+                                            public void onResponse(String response) {
+                                                try {
+                                                    JSONObject jsonObject = new JSONObject(response);
+                                                    String success = jsonObject.getString("success");
+                                                    JSONArray jsonArray = jsonObject.getJSONArray("read");
+
+                                                    if (success.equals("1")) {
+                                                        for (int i = 0; i < jsonArray.length(); i++) {
+                                                            JSONObject object = jsonArray.getJSONObject(i);
+
+                                                            String ticket_status = object.getString("active").trim();
+                                                            String ticket_eventid = object.getString("event_id").trim();
+
+                                                            if (ticket_status.equals("1") && ticket_eventid.equals(getArguments().getString(ARG_ITEM_ID))) {
+                                                                resultText.setTextColor(ContextCompat.getColor(getContext(), R.color.ticket_valid));
+                                                                resultText.setText(getString(R.string.ticket_valid));
+                                                                Toast.makeText(getContext(), "Ticket valid" + "\n" + "Ticket status: " + ticket_status + "\n" + "Event id: " + getArguments().getString(ARG_ITEM_ID) + "\n" + "Ticket event_id: " + ticket_eventid + "\n" + "Ticket key: " + qrCodes.valueAt(0).displayValue, Toast.LENGTH_LONG).show();
+                                                            } else {
+                                                                resultText.setTextColor(ContextCompat.getColor(getContext(), R.color.ticket_invalid));
+                                                                resultText.setText(getString(R.string.ticket_invalid));
+                                                                Toast.makeText(getContext(), "Ticket invalid" + "\n" + "Ticket status: " + ticket_status + "\n" + "Event id: " + getArguments().getString(ARG_ITEM_ID) + "\n" + "Ticket event_id: " + ticket_eventid + "\n" + "Ticket key: " + qrCodes.valueAt(0).displayValue, Toast.LENGTH_LONG).show();
+                                                            }
+                                                        }
+                                                    }
+                                                } catch (JSONException e) {
+                                                    e.printStackTrace();
+                                                    Toast.makeText(getContext(), "Cannot get ticket. Error: " + e.toString(), Toast.LENGTH_LONG).show();
+                                                }
+                                            }
+                                        },
+                                        new Response.ErrorListener() {
+                                            @Override
+                                            public void onErrorResponse(VolleyError error) {
+                                                Toast.makeText(getContext(), "Cannot get ticket. Error: " + error.toString(), Toast.LENGTH_LONG).show();
+                                            }
+                                        }) {
+                                    @Override
+                                    protected Map<String, String> getParams() throws AuthFailureError {
+                                        Map<String, String> params = new HashMap<>();
+                                        params.put("key", qrCodes.valueAt(0).rawValue);
+                                        return params;
+                                    }
+                                };
+
+                                RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+                                requestQueue.add(stringRequest);
+                                lastScannedKey = qrCodes.valueAt(0).rawValue;
+                            }
+                        });
+                    }
+                }
+            }
+        });
 
         return rootView;
     }
